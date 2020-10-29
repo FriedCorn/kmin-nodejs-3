af09e92cae424a926040afa459f991d61955de30
FriedCorn <khoa.ngobk@hcmut.edu.vn>
2020-10-29 20:25:37 +0700

    OK

--- weather-app/src/server.js
+++ weather-app/src/server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs());
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
