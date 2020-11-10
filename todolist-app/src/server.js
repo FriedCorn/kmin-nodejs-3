361a41877678912e516574f5a59cac7269e68a6e
FriedCorn <khoa.ngobk@hcmut.edu.vn>
2020-11-10 17:16:07 +0700

    Continue todolist-app

--- todolist-app/src/server.js
+++ todolist-app/src/server.js
@@ -1,31 +1,50 @@
 const express = require('express');
-const { createTask, getAllTasks, getTaskById } = require('./services/task-service');
+const { createTask, getAllTasks, getTaskById, updateTask, deleteTask } = require('./services/task-service');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 
-app.post("/tasks", (req, res) => {
-    const { title, body } = req.body;
-    const createdTask = createTask({ title, body });
-    res.send(createdTask);
-});
+app.route("/tasks")
+    .post((req, res) => {
+        const { title, body } = req.body;
 
-app.get("/tasks", (req, res) => {
-    const allTask = getAllTasks();
-    if (allTask.length != 0)
-        res.send(allTask);
-    else
-        res.send("NO TASK");
-});
+        if (!title) {
+            return res.status(400).json({ message: "title is required" });
+        }
+        const createdTask = createTask({ title, body });
+        res.send(createdTask);
+    })
+    .get((req, res) => {
+        const allTask = getAllTasks();
+        if (allTask.length != 0)
+            res.send(allTask);
+        else
+            res.send("NO TASK");
+    });
 
-app.get("/tasks/:taskId", (req, res) => {
-    const task = getTaskById(req.params.taskId);
-    if (task != null)
-        res.send(task);
-    else
-        res.send("NOT FOUND");
-})
+app.route("/tasks/:taskId")
+    .get((req, res) => {
+        const foundTask = getTaskById(req.params.taskId);
+        if (foundTask != null)
+            res.send(foundTask);
+        else
+            res.send("NOT FOUND");
+    })
+    .patch((req, res) => {
+        const { title, body, completed } = req.body;
+        const updatedTask = updateTask(req.params.taskId, { title, body, completed });
+        if (updateTask != null)
+            res.send(updatedTask);
+        else
+            res.send("Fail Updating");
+    })
+    .delete((req, res) => {
+        if (deleteTask(req.params.taskId))
+            res.send("Deleted");
+        else
+            res.send("Fail");
+    });
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
--- todolist-app/src/services/task-service.js
+++ todolist-app/src/services/task-service.js
@@ -4,7 +4,17 @@ const Task = createModel('tasks');
 
 // Create task
 function createTask({ title, body }) {
-    return Task.create({title, body});
+    const tasks = Task.findAll();
+    const lastId = tasks.length + 1;
+    const newTask = {
+        id: lastId,
+        title,
+        body,
+        createdDate: new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString(),
+        completed: false,
+        completedAt: null,
+    };
+    return Task.create(newTask);
 }
 
 // Get all tasks
@@ -18,8 +28,45 @@ function getTaskById(taskId) {
     return foundTask;
 }
 
+function updateTask(taskId, { title, body, completed }) {
+    const foundTask = Task.findById(taskId);
+    if (foundTask == null)
+        return null;
+
+    let checkUpdate = false;
+    if (foundTask.title != title && title) {
+        foundTask.title = title;
+        checkUpdate = true;
+    }
+    if (foundTask.body != body && body) {
+        foundTask.body = body;
+        checkUpdate = true;
+    }
+    if (foundTask.completed != completed) {
+        foundTask.completed = completed;
+        if (completed == true)
+            foundTask.completedAt = new Date().toLocaleTimeString();
+        else
+            foundTask.completedAt = null;
+        checkUpdate = true;
+    }
+
+    if (checkUpdate == true)
+        return Task.update(foundTask);
+    return null;
+}
+
+function deleteTask(taskId) {
+    const task = Task.findById(taskId);
+    // if (task)
+        return Task.removeById(taskId);
+    // return null;
+}
+
 module.exports = {
     createTask,
     getAllTasks,
     getTaskById,
+    updateTask,
+    deleteTask
 };
