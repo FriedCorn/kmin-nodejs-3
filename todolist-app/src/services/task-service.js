b06148f79f525d7560e33c3fb72b295d9efead88
FriedCorn <khoa.ngobk@hcmut.edu.vn>
2020-11-19 14:25:34 +0700

    Modify to mongoose

--- todolist-app/src/services/task-service.js
+++ todolist-app/src/services/task-service.js
@@ -5,16 +5,6 @@ const TaskJSONDB = createModel('tasks');
 
 // Create task
 function createTask({ title, body }) {
-    // const tasks = TaskJSONDB.findAll();
-    // const newTask = {
-    //     id: Date.now().toString(),
-    //     title,
-    //     body,
-    //     createdAt: new Date(),
-    //     completed: false,
-    //     completedAt: null,
-    // };
-    // return TaskJSONDB.create(newTask);
     return new Promise((resolve, reject) => {
         Task.create(
             {
@@ -45,8 +35,6 @@ function getAllTasks() {
 
 // Get task by ID
 function getTaskById(taskId) {
-    // const foundTask = TaskJSONDB.findById(taskId);
-    // return foundTask;
     return new Promise((resolve, reject) => {
         Task.findById(taskId)
             .then((task) => {
@@ -59,31 +47,6 @@ function getTaskById(taskId) {
 }
 
 function updateTask(taskId, { title, body, completed }) {
-    // const foundTask = TaskJSONDB.findById(taskId);
-    // if (foundTask == null)
-    //     return null;
-
-    // let checkUpdate = false;
-    // if (foundTask.title != title && title) {
-    //     foundTask.title = title;
-    //     checkUpdate = true;
-    // }
-    // if (foundTask.body != body && body) {
-    //     foundTask.body = body;
-    //     checkUpdate = true;
-    // }
-    // if (foundTask.completed != completed) {
-    //     foundTask.completed = completed;
-    //     if (completed == true)
-    //         foundTask.completedAt = new Date();
-    //     else
-    //         foundTask.completedAt = null;
-    //     checkUpdate = true;
-    // }
-
-    // if (checkUpdate == true)
-    //     return TaskJSONDB.update(foundTask);
-    // return null;
     return new Promise((resolve, reject) => {
         Task.findById(taskId)
             .then((foundTask) => {
@@ -103,10 +66,6 @@ function updateTask(taskId, { title, body, completed }) {
 }
 
 function deleteTaskById(taskId) {
-    // const task = TaskJSONDB.findById(taskId);
-    // // if (task)
-    // return TaskJSONDB.removeById(taskId);
-    // // return null;
     return new Promise((resolve, reject) => {
         Task.findByIdAndRemove(taskId)
             .then((task) => {
